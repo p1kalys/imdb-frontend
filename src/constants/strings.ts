e7245bef91a73ec56089abde5d06a310b9187163
Pavan Emani <pavanemani14@gmail.com>
2024-07-09 09:30:18 +0530

    new

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,19 +1,19 @@
-export const HOME_ENDPOINT = "/"
-export const LOGIN_ENDPOINT = "/auth/login"
-export const SIGNUP_ENDPOINT = "/auth/register"
-export const IMDB_MOVIES_ENDPOINT = "/imdb-movies"
-export const SAVED_MOVIES_ENDPOINT = "/saved-movies"
-export const ADD_MOVIES_ENDPOINT = "/add-movies"
-export const EDIT_MOVIES_ENDPOINT = "/movie/edit"
-export const PROFILE_ENDPOINT = "/profile"
+export const HOME_ENDPOINT = "/api/"
+export const LOGIN_ENDPOINT = "/api/auth/login"
+export const SIGNUP_ENDPOINT = "/api/auth/register"
+export const IMDB_MOVIES_ENDPOINT = "/api/imdb-movies"
+export const SAVED_MOVIES_ENDPOINT = "api/saved-movies"
+export const ADD_MOVIES_ENDPOINT = "/api/add-movies"
+export const EDIT_MOVIES_ENDPOINT = "/api/movie/edit"
+export const PROFILE_ENDPOINT = "/api/profile"
 
 // Api end points
 
-export const ALL_ACTORS_ENDPOINT = "/actor/all"
-export const INSERT_ACTOR_ENDPOINT = "/actor/insert"
+export const ALL_ACTORS_ENDPOINT = "/api/actor/all"
+export const INSERT_ACTOR_ENDPOINT = "/api/actor/insert"
 
-export const ALL_PRODUCER_ENDPOINT = "/producer/all"
-export const INSERT_PRODUCER_ENDPOINT = "/producer/insert"
+export const ALL_PRODUCER_ENDPOINT = "/api/producer/all"
+export const INSERT_PRODUCER_ENDPOINT = "/api/producer/insert"
 
-export const MOVIES_BY_USERID_ENDPOINT = "/movie/all"
-export const INSERT_MOVIE_ENDPOINT = "/movie/insert"
+export const MOVIES_BY_USERID_ENDPOINT = "/api/movie/all"
+export const INSERT_MOVIE_ENDPOINT = "/api/movie/insert"
